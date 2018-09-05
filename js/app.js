0df4db8e6c048aea07944527e3931a37d8c05d72
Alejandro <alejogtunit460@gmail.com>
2018-09-04 22:07:02 -0600

    Avances

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ app.config(function($routeProvider){
     .when("/", { templateUrl: "views/main.html" })
     .when("/picks-gratis", { templateUrl: "views/especificos/picks-gratis.html" })
     .when("/productos-servicios", { templateUrl: "views/productos-servicios.html" })
+    .when("/genericos", { templateUrl: "views/genericos.html" })
     .when("/noticias", { templateUrl: "views/noticias.html" })
     .when("/contacto", { templateUrl: "views/contacto.html" });
 });
